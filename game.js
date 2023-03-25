c95ce81f1b75a5fe83ef95daa821c6d72f0ca641
tomkimberlin <6531977+tomkimberlin@users.noreply.github.com>
2023-03-24 23:10:14 -0500

    refactor game.js

--- game.js
+++ game.js
@@ -11,27 +11,16 @@ const config = {
         },
     },
     scene: {
-        preload: preload,
-        create: create,
-        update: update,
+        preload,
+        create,
+        update,
     },
 };
 
-window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-    game.scale.resize(newWidth, newHeight);
-    startText.setPosition(newWidth / 2, newHeight / 2); // Update startText position when the window is resized
-});
-
 const game = new Phaser.Game(config);
-let bird;
-let pipes;
-let pipeTimer;
-let score = 0;
-let scoreText;
-let gameStarted = false; // Added gameStarted variable
-let startText; // Added startText variable
+let bird, pipes, pipeTimer, score = 0, scoreText, gameStarted = false, startText;
+
+window.addEventListener('resize', resizeGame);
 
 function preload() {
     this.load.image('bird', 'assets/bird.png');
@@ -39,74 +28,67 @@ function preload() {
 }
 
 function create() {
-    const fontSize = Math.round(Math.min(config.width, config.height) * 0.03); // Calculate the font size
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: fontSize + 'px', fill: '#000' });
-    scoreText.setDepth(1);
-
-    const birdStartingOffset = 40; // You can adjust this value to change the bird's starting position
-    bird = this.physics.add.sprite(birdStartingOffset, config.height / 2, 'bird').setScale(1);
-    bird.setCollideWorldBounds(true);
-    bird.body.allowGravity = false; // Disable gravity initially
-
+    createScoreText.call(this);
+    createBird.call(this);
     pipes = this.physics.add.group();
-
-    this.input.on('pointerdown', () => {
-        if (!gameStarted) {
-            gameStarted = true;
-            addPipes.call(this);
-            bird.body.allowGravity = true; // Enable gravity when the game starts
-            startText.setVisible(false); // Hide startText
-            pipeTimer = this.time.addEvent({ delay: 2000, callback: addPipes, callbackScope: this, loop: true }); // Start pipeTimer after the game starts
-        }
-        bird.setVelocityY(-350);
-    });
-
-    this.physics.add.collider(bird, pipes, gameOver, null, this);
-
-    // Added startText
-    startText = this.add.text(config.width / 2, config.height / 2, 'Tap or click to start', { fontSize: fontSize * 1.5 + 'px', fill: '#000' });
-    startText.setOrigin(0.5); // Center the startText
+    createInputListener.call(this);
+    createCollisionListener.call(this);
+    createStartText.call(this);
 }
 
 function update() {
-    if (!gameStarted) {
-        return;
-    }
+    if (!gameStarted) return;
+    checkGameOver.call(this);
+    removeOffscreenPipes();
+    incrementScore();
+    updateBirdRotation();
+}
 
+// Utility functions
+
+function resizeGame() {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    game.scale.resize(newWidth, newHeight);
+    startText.setPosition(newWidth / 2, newHeight / 2);
+}
+
+function createScoreText() {
+    const fontSize = Math.round(Math.min(config.width, config.height) * 0.03);
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: `${fontSize}px`, fill: '#000' });
+    scoreText.setDepth(1);
+}
+
+function createBird() {
+    const birdStartingOffset = 40;
+    bird = this.physics.add.sprite(birdStartingOffset, config.height / 2, 'bird').setScale(1);
+    bird.setCollideWorldBounds(true);
+    bird.body.allowGravity = false;
+}
+
+function createInputListener() {
+    this.input.on('pointerdown', () => {
+        if (!gameStarted) {
+            startGame.call(this);
+        }
+        bird.setVelocityY(-350);
+    });
+}
+
+function createCollisionListener() {
+    this.physics.add.collider(bird, pipes, gameOver, null, this);
+}
+
+function createStartText() {
+    const fontSize = Math.round(Math.min(config.width, config.height) * 0.045);
+    startText = this.add.text(config.width / 2, config.height / 2, 'Tap or click to start', { fontSize: `${fontSize}px`, fill: '#000' });
+    startText.setOrigin(0.5);
+}
+
+function checkGameOver() {
     if (bird.y < 0 || bird.y >= config.height - bird.displayHeight) {
         gameOver.call(this);
     }
-    removeOffscreenPipes();
-    incrementScore();
-
-    const birdRotation = Phaser.Math.Clamp(bird.body.velocity.y / 1000, -0.15, 0.6);
-    bird.setRotation(birdRotation * Math.PI);
-}
-
-function addPipes() {
-    const screenWidth = game.scale.width;
-    const screenHeight = game.scale.height;
-
-    const pipeHoleHeight = screenHeight * 0.25;
-    const minPipeHeight = screenHeight * 0.1;
-    const maxPipeHeight = screenHeight * 0.75 - pipeHoleHeight;
-    const pipeHeight = Phaser.Math.Between(minPipeHeight, maxPipeHeight);
-
-    const pipeWidth = screenWidth / 8;
-
-    const upperPipe = pipes.create(screenWidth, 0, 'pipe');
-    upperPipe.setOrigin(0, 0);
-    upperPipe.body.allowGravity = false;
-    upperPipe.setVelocityX(-200);
-    upperPipe.setDisplaySize(pipeWidth, pipeHeight);
-
-    const lowerPipeY = pipeHeight + pipeHoleHeight;
-    const lowerPipeHeight = screenHeight - lowerPipeY;
-    const lowerPipe = pipes.create(screenWidth, screenHeight, 'pipe');
-    lowerPipe.setOrigin(0, 1); // Set the origin to the bottom left corner
-    lowerPipe.body.allowGravity = false;
-    lowerPipe.setVelocityX(-200);
-    lowerPipe.setDisplaySize(pipeWidth, lowerPipeHeight);
 }
 
 function removeOffscreenPipes() {
@@ -119,26 +101,63 @@ function removeOffscreenPipes() {
 
 function incrementScore() {
     pipes.children.each(pipe => {
-        // Check if the pipe is an upper pipe by comparing its origin
         const isUpperPipe = pipe.originY === 0;
         if (isUpperPipe && pipe.x + pipe.displayWidth < bird.x && !pipe.scored) {
             pipe.scored = true;
             score++;
-            scoreText.setText('score: ' + score);
+            scoreText.setText(`score: ${score}`);
         }
     });
+}
+
+function updateBirdRotation() {
+    const birdRotation = Phaser.Math.Clamp(bird.body.velocity.y / 1000, -0.15, 0.6);
+    bird.setRotation(birdRotation * Math.PI);
+}
+
+function addPipes() {
+    const screenWidth = game.scale.width;
+    const screenHeight = game.scale.height;
+    const pipeHoleHeight = screenHeight * 0.25;
+    const minPipeHeight = screenHeight * 0.1;
+    const maxPipeHeight = screenHeight * 0.75 - pipeHoleHeight;
+    const pipeHeight = Phaser.Math.Between(minPipeHeight, maxPipeHeight);
+    const pipeWidth = screenWidth / 8;
+
+    createPipe(screenWidth, 0, pipeWidth, pipeHeight, 0, 0);
+    createPipe(screenWidth, screenHeight, pipeWidth, screenHeight - (pipeHeight + pipeHoleHeight), 0, 1);
+}
+
+function createPipe(x, y, width, height, originX, originY) {
+    const pipe = pipes.create(x, y, 'pipe');
+    pipe.setOrigin(originX, originY);
+    pipe.body.allowGravity = false;
+    pipe.setVelocityX(-200);
+    pipe.setDisplaySize(width, height);
+}
+
+function startGame() {
+    gameStarted = true;
+    bird.body.allowGravity = true;
+    startText.setVisible(false);
+    addPipes.call(this);
+    pipeTimer = this.time.addEvent({ delay: 2000, callback: addPipes, callbackScope: this, loop: true });
 }
 
 function gameOver() {
     pipeTimer.remove();
     pipes.clear(true, true);
-    bird.setActive(false);
-    bird.body.allowGravity = false; // Disable gravity after game over
-    bird.setY(config.height / 2); // Center the bird vertically after game over
-    bird.setVelocity(0, 0); // Reset bird's velocity
-    bird.setRotation(0); // Reset bird's rotation
+    resetBird();
     score = 0;
-    scoreText.setText('score: ' + score); // Update score display text
-    gameStarted = false; // Reset gameStarted
-    startText.setVisible(true); // Show startText
+    scoreText.setText(`score: ${score}`);
+    gameStarted = false;
+    startText.setVisible(true);
+}
+
+function resetBird() {
+    bird.setActive(false);
+    bird.body.allowGravity = false;
+    bird.setY(config.height / 2);
+    bird.setVelocity(0, 0);
+    bird.setRotation(0);
 }
